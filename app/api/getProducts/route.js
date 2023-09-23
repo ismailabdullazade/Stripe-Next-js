51d373b2251ebd6e0c72e8da07efcb300d4482d0
IsmailGN <84373556+IsmailGN@users.noreply.github.com>
2023-09-23 16:08:07 +0400

    created GET request

---
+++ app/api/getProducts/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+
+export async function GET(request){
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+    const prices = await stripe.prices.list({
+        limit: 4
+    });
+
+    return NextResponse.json(prices.data.reverse())
+}
